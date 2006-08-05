c16c3d91b7553718da80288b1dedd9cf20dcbb5a
marr99 <marr99>
2006-08-05 02:02:48 +0000

    Changes for new 2.0.0 release.

--- lcaldefs.h
+++ lcaldefs.h
@@ -1,137 +1,114 @@
-/*
- * lcaldefs.h - definitions for Lcal (lunar calendar) program
- *
+/* ---------------------------------------------------------------------------
+
+   lcaldefs.h
+   
+   Notes:
+
+      This file contains various definitions and declarations of items used
+      throughout the application.
+     
+   Revision history:
+
+	2.0.0
+		B.Marr		2006-07-20
+		
+		Reformatted comments and code to match my standards.
+		
+		B.Marr		2006-06-24
+		
+		Major overhaul.  Eliminated lots of unnecessary items.
+		Cleaned up lots of other items.
+		
+		Merged 'lcallang.h' and 'protos.h' into this file.
+		
+		Added a defined constant (string) to represent the
+		'lcal'/'pcal' website in the PostScript output.
+		
+*/
+
+
+/* ---------------------------------------------------------------------------
+   
+   Notes:
+
+      This file contains various definitions and function prototypes.
+
+*/
+
+/* ---------------------------------------------------------------------------
+
+   Header Files
+
+*/
+
+/* ---------------------------------------------------------------------------
+
+   Type, Struct, & Enum Declarations
+
+*/
+
+/* 
+ * Global typedef declaration for 'flag usage' (cf. lcal.c, get_args())
  */
+typedef struct {
+   char flag;   /* name of flag */
+   char has_arg;   /* TRUE if flag takes (optional) arg */
+} flag_usage_str_typ;
+
+/*
+ * Global typedef declarations for 'usage message' (cf. lcal.c, usage())
+ */
+typedef struct {
+   char flag;   /* name of flag */
+   char *meta;   /* metaname for argument (if any) */
+   char *text;   /* associated text */
+   char *def;   /* default value (if any) */
+} flag_msg_str_typ;
+
+typedef struct {
+   char *desc;   /* description of param */
+   char *text;   /* associated text */
+} param_msg_str_typ;
+
+/* ---------------------------------------------------------------------------
+
+   Constant Declarations
+
+*/
+
+/* Define strings for comments in PostScript output file... */
+#define PS_RELEASE      "PS-Adobe-1.0"
+#define LCAL_WEBSITE    "http://pcal.sourceforge.net"
 
 /*
  * System dependencies:
  */
-
-#ifdef VMS		/* VMS oddities isolated here */
-
-#include <ssdef.h>	/* required for trnlog() */
-#include <descrip.h>
-
-#define DEFAULT_OUTFILE	"calendar.ps"
-
-#define EXIT_SUCCESS	1
-#define EXIT_FAILURE	3
-
+#ifdef BUILD_ENV_MSDOS
+/* oddities for MS-DOS */
+#define END_PATH   '\\'
 #else
-#ifdef AMIGA		/* more oddities for Amiga */
+/* not MS-DOS -- assume Unix or DOS+DJGPP */
+#define END_PATH   '/'
 
-#include <stdlib.h>
-#include <string.h>
-
-#define PROTOS				/* compiler accepts prototypes */
-#define DEFAULT_OUTFILE	"RAM:calendar.ps"
-
-#else
-#ifdef DOS		/* even more oddities for MS-DOS */
-
-#else			/* neither VMS, Amiga, nor MS-DOS - assume Un*x */
-
-#define UNIXX		/* to distinguish from others (not used yet) */
-#include <unistd.h>
-
-#define END_PATH	'/'
-
-#define PAGER_ENV	"PAGER"		/* points to help message pager */
-#define PAGER_DEFAULT	"more"		/* default pager (NULL = none) */
-
-#endif
-#endif
+/* 
+   PAGER_ENV -- points to help message pager
+   PAGER_DEFAULT -- default pager (NULL = none)
+*/
+#define PAGER_ENV "PAGER"
+#define PAGER_DEFAULT "more"
 #endif
 
-/* define OUTFILE to DEFAULT_OUTFILE if defined, otherwise to "" (stdout) */
-
-#ifdef DEFAULT_OUTFILE
-#define OUTFILE		DEFAULT_OUTFILE
-#else
-#define OUTFILE		""
-#endif
-
-/* PROTOS may be defined independently of __STDC__ for compilers which
- * support function prototypes but are not fully ANSI-compliant
- */
-
-#if defined(__STDC__) && ! defined(PROTOS)
-#define PROTOS	
-#endif
-
-/* STDLIB may be defined independently of __STDC__ for systems which
- * support <stdlib.h> but are not fully ANSI-compliant
- */
-
-#if defined(__STDC__) && ! defined(STDLIB)
-#define STDLIB                  /* cf. {no}protos.h */
-#endif
-
-#ifdef STDLIB                   /* include <stdlib.h> if known to exist */
-#include <stdlib.h>
-#endif
-
-/* EXIT_SUCCESS and EXIT_FAILURE should be defined in <stdlib.h>
- * (or above if non-standard); define here if necessary
- */
-
-#ifndef EXIT_SUCCESS
-#define EXIT_SUCCESS    0
-#endif
-#ifndef EXIT_FAILURE
-#define EXIT_FAILURE    1
-#endif
-
-/* definition of NULL (if needed) */
- 
-#ifndef NULL
-#define NULL	0
-#endif
+/* define OUTFILE to "" (stdout) */
+#define OUTFILE   ""
 
 /*
- * Environment variables (global symbol, logical name on VMS):
+ * Environment variables:
  */
-
-#define LCAL_OPTS	"LCAL_OPTS"	/* command-line flags */
-
-/*
- * Function-like macros:
- */
-
-#define FPR		(void)fprintf
-#define PRT		(void)printf
-
-#define IS_LEAP(y)	((y) % 4 == 0 && ((y) % 100 != 0 || (y) % 400 == 0))
-#define LENGTH_OF(m, y) (month_len[(m)-1] + ((m) == FEB && IS_LEAP(y)))
-#define YEAR_LEN(y)	(IS_LEAP(y) ? 366 : 365)
-#define DAY_OF_YEAR(m, d, y) ((month_off[(m)-1] + ((m) > FEB && IS_LEAP(y))) + d)
-#define OFFSET_OF(m, y) ((month_off[(m)-1] + ((m) > FEB && IS_LEAP(y))) % 7)
-#define FIRST_OF(m, y)	calc_weekday(m, 1, y)
-
-#define P_LASTCHAR(p)	((p) && *(p) ? (p) + strlen(p) - 1 : NULL)
-#define LASTCHAR(p)	(p)[strlen(p) - 1]
-
-#define IS_NUMERIC(p)	((p)[strspn((p), DIGITS)] == '\0')
-
-#define ERR(errmsg) \
-	FPR(stderr, E_ILL_LINE, progname, errmsg, filename, line);
-
-#ifdef __STDC__
-#define TOLOWER(c)	tolower(c)
-#else
-#define TOLOWER(c)	(isupper(c) ? tolower(c) : (c))
-#endif
-
+#define LCAL_OPTS   "LCAL_OPTS"   /* command-line flags */
 
 /*
  * Miscellaneous other constants:
  */
-
-#ifdef EPS
-#define PS_RELEASE	"PS-Adobe-2.0 EPSF"	/* for comments at top */
-#else
-#define PS_RELEASE	"PS-Adobe-1.0"		/* for comments at top */
-#endif
 
 #define P_ENV		1	/* environment variable / command line pass */
 #define P_CMD1		2
@@ -168,14 +145,26 @@
  * Defaults for calendar layout:
  */
 
+/* default font names */
 #ifndef DATEFONT
-#define DATEFONT	"Times-Bold"	/* default font names */
+#define DATEFONT   "Times-Bold"
 #endif
 #ifndef TITLEFONT
-#define TITLEFONT	"Times-Bold"
+#define TITLEFONT   "Times-Bold"
 #endif
 
-#define	TITLEFONTSIZE	72		/* font size for year...	*/
+/* 
+
+   Define the various font sizes... 
+
+*/
+
+/* The title font (for the year of the calendar) must be made much smaller
+   (and relocated) to fit everything on one page when the odd-days-only ('-O')
+   method of single-page output has been enabled... */
+#define	TITLEFONTSIZE_NORMAL	72
+#define	TITLEFONTSIZE_ODD_DAYS	18
+
 #define	DATEFONTSIZE	16		/* dates			*/
 #define	MONTHFONTSIZE	16		/* month names			*/
 #define	WKDFONTSIZE	10		/* weekday names (inside moons) */
@@ -186,9 +175,9 @@
 #define SMWKDFONTSIZE_S	 9		/* is compressed (-S)		*/
 
 /*
- * very few PS printers will actually print to the edges of each page; these
- * site-specific magic numbers (also definable in the Makefile) compensate
- * for this
+ * Very few PostScript printers will actually print to the edges of each page.
+ * These site-specific magic numbers (also definable in the Makefile)
+ * compensate for this.
  */
 #ifndef X_OFFSET
 #define X_OFFSET	"0/0"		/* X origin offset (p.1, p.2) */
@@ -197,9 +186,9 @@
 #define Y_OFFSET	"0/0"		/* Y origin offset (p.1, p.2) */
 #endif
 
-#define LANDSCAPE  	90		/* degrees to rotate */
-#define PORTRAIT    	 0
-#define ROTATE	   	LANDSCAPE	/* default */
+#define LANDSCAPE	90		/* degrees to rotate */
+#define PORTRAIT	 0
+#define ROTATE		LANDSCAPE	/* default */
 
 #define WEEKDAYS	FALSE		/* weekdays inside moons? */
 
@@ -224,29 +213,115 @@
 #define INVERT		"0/1"		/* -i flag: white-on-black */
 #define RGB_CHAR	':'		/* r:g:b value separator */
 
-/* 
- * Global typedef declaration for flag usage struct (cf. lcallang.h, get_args())
- */
 
-typedef struct {
-	char flag;		/* name of flag */
-	char has_arg;		/* TRUE if flag takes (optional) arg */
-	} FLAG_USAGE;
+/* size of abbreviations  */
+#define MIN_DAY_LEN	2
+#define MIN_MONTH_LEN	3
+
 
 /*
- * Global typedef declarations for usage message (cf. lcallang.h, usage())
+ * Symbolic names for command-line flags.  These may be changed
+ * as desired in order to be meaningful in languages other than
+ * English.
+ */
+#define F_DAY_FONT	'd'		/* select alternate day font */
+#define F_TITLE_FONT	't'		/* select alternate title font */
+
+#define F_OUT_FILE	'o'		/* select alternate output file */
+
+#define F_LANDSCAPE	'l'		/* landscape mode */
+#define F_PORTRAIT	'p'		/* portrait mode */
+
+#define F_SHADING	's'		/* set background/foreground shading */
+
+#define F_INVERT	'i'		/* print white on black background */
+
+#define F_HELP		'h'		/* generate help message */
+#define F_USAGE		'u'		/* generate usage message */
+#define F_VERSION	'v'		/* generate version stamp */
+
+#define F_WEEKDAYS	'W'		/* weekday names inside moons */
+
+#define F_XOFFSET	'X'		/* fudge factor for X offset (-l) */
+#define F_YOFFSET	'Y'		/* fudge factor for Y offset (-p) */
+
+#define F_TIMEZONE	'z'		/* specify alternate time zone */
+
+#define F_COMPR_1PAGE	'S'		/* print compressed, single-page calendar */
+#define F_ODD_DAYS_1PAGE	'O'	/* print odd-days-only, single-page calendar */
+
+#define W_FONT		"<FONT>"		/* names of metavariables */
+#define W_FILE		"<FILE>"
+#define W_VALUE		"<VALUE>"
+#define W_VAL2		"<n>{/<n>}"
+#define W_SHADING	"{<b>}{/<f>}"
+
+/* special flag_msg[] entries for end of option group, etc. */
+
+#define END_GROUP	'\n', NULL, NULL, NULL		/* end of option group */
+#define END_LIST	'\0', NULL, NULL, NULL		/* end of list */
+#define GROUP_DEFAULT	' ', NULL, " "			/* group default */
+
+/* Define the number of entries from the 'param_msg[]' array to print in
+   command-line syntax message */
+#define PARAM_MSGS	1
+
+/* Error and information messages - translate as necessary */
+
+/* program error messages */
+#define	E_FOPEN_ERR	"%s: can't open file %s\n"
+#define	E_ILL_OPT	"%s: unrecognized flag %s"
+#define E_ILL_OPT2	" (%s\"%s\")"
+#define	E_ILL_YEAR	"%s: year %d not in range %d .. %d\n"
+#define E_FLAG_IGNORED	"%s: -%c flag ignored (%s\"%s\")\n"
+#define ENV_VAR		"environment variable "
+
+/* ---------------------------------------------------------------------------
+
+   Macro Definitions
+   
+*/
+
+/* 
+ * Define a 'convenience' macro used to explicitly declare arguments as
+ * 'unused' where intentionally-unused function parameters are declared.  This
+ * prevents warnings by the 'gcc' GNU C compiler that would otherwise appear
+ * because of using the '-W' compile option.
+ *
+ * The Unix and DOS+DJGPP build environments support this ability.  We assume
+ * that all other build environments do not support it, so this macro is
+ * defined as a null value in such cases.
+ */ 
+#if defined (BUILD_ENV_UNIX) || defined (BUILD_ENV_DJGPP)
+#define GCC_UNUSED  __attribute__ ((unused))
+#else
+#define GCC_UNUSED
+#endif
+
+/*
+ * Function-like macros:
  */
 
-typedef struct {
-	char flag;		/* name of flag */
-	char *meta;		/* metaname for argument (if any) */
-	char *text;		/* associated text */
-	char *def;		/* default value (if any) */
-	} FLAG_MSG;
+#define IS_LEAP(y)   ((y) % 4 == 0 && ((y) % 100 != 0 || (y) % 400 == 0))
+#define LENGTH_OF(m, y) (month_len[(m)-1] + ((m) == FEB && IS_LEAP(y)))
+#define YEAR_LEN(y)   (IS_LEAP(y) ? 366 : 365)
+#define DAY_OF_YEAR(m, d, y) ((month_off[(m)-1] + ((m) > FEB && IS_LEAP(y))) + d)
+#define OFFSET_OF(m, y) ((month_off[(m)-1] + ((m) > FEB && IS_LEAP(y))) % 7)
+#define FIRST_OF(m, y)   calc_weekday(m, 1, y)
 
-typedef struct {
-	char *desc;		/* description of param */
-	char *text;		/* associated text */
-	} PARAM_MSG;
+#define P_LASTCHAR(p)   ((p) && *(p) ? (p) + strlen(p) - 1 : NULL)
+#define LASTCHAR(p)   (p)[strlen(p) - 1]
 
+#define IS_NUMERIC(p)   ((p)[strspn((p), DIGITS)] == '\0')
 
+/* ---------------------------------------------------------------------------
+
+   Data Declarations (including externals)
+
+*/
+
+/* ---------------------------------------------------------------------------
+
+   External Routine References & Function Prototypes
+
+*/
